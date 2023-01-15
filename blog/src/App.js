91db92f744cade2020d9c5f8fd7a36ba77be36e4
Jonathan <jonathan.martinmaestre71@gmail.com>
2023-01-15 17:02:45 +0100

    first tutorial, added few components and libraries

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+  
+import Posts from "./components/Posts/Posts";
+  
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Blog
-        </p>
-      </header>
+    <div className="main-container">
+      <h1 className="main-heading">
+        Blog App using React Js 
+      </h1>
+      <Posts />
     </div>
   );
-}
-
+};
+  
 export default App;
